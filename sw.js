7b2b3c756b86add0854de3287a27a3c1ee446814
lavinma <lavin.ma18@gmail.com>
2025-05-26 14:43:47 -0700

    fixed file paths

--- sw.js
+++ sw.js
@@ -30,12 +30,12 @@ self.addEventListener('install', function (event) {
         './assets/images/icons/icon-256x256.png',
         './assets/images/icons/icon-384x384.png',
         './assets/images/icons/icon-512x512.png',
-        './recipes/https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
-        './recipes/https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
-        './recipes/https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
-        './recipes/https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
-        './recipes/https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
-        './recipes/https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json'
+        'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json'
       ]);
     })
   );
